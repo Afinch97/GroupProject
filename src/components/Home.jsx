e0f2e6292f5205c1633e17a72bda462e86369b62
Afinch97 <49850011+Afinch97@users.noreply.github.com>
2022-03-31 19:23:56 -0400

    getting started

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,8 @@
 import React, {useState} from "react";
 import {useNavigate} from "react-router-dom";
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+import "./homeStyle.css";
 
 const Home = ()=>{
     const [user, setUser] = useState({username:"", password:"", remember:false});
@@ -25,22 +28,27 @@ const Home = ()=>{
         }
         
         return (
-            <>
-            <div className="error"><p>{error}</p></div>
-            <form  onSubmit={Submit}>
-                <label htmlFor="user">Name: </label>
-                <input type="text" name="user" id="user" onChange={e =>setUser({...user, username: e.target.value})} value={user.username}/>
-
-                <label htmlFor="user">Password: </label>
-                <input type="password" name="password" id="password" onChange={e =>setUser({...user, password: e.target.value})} value={user.password} />
-
-                <label className="checkbox">
-                    <input type="checkbox" name="remember" value={user.remember} onChange={e =>setUser({...user, remember: !user.remember})} />
-                    Remember me
-                </label>
-                <input type="submit" name="Sign Up" />
+            <div className="inner">
+            <form onSubmit={Submit}>
+                <h3>Sign In</h3>
+                <div className="error"><p>{error}</p></div>
+                <div className="form-group">
+                    <label htmlFor="user">Name: </label>
+                    <input type="text" className="form-control" name="user" id="user" placeholder="Enter Username or Email" onChange={e =>setUser({...user, username: e.target.value})} value={user.username}/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="user">Password: </label>
+                    <input type="password" className="form-control" placeholder="Password" name="password" id="password" onChange={e =>setUser({...user, password: e.target.value})} value={user.password} />
+                </div>
+                <div className="form-group">
+                    <div className="custom-control custom-checkbox">
+                        <input type="checkbox" className="custom-control-input" id="remember" value={user.remember} onChange={e =>setUser({...user, remember: !user.remember})} />
+                        <label className="custom-control-label" htmlFor="remember">Remember me</label>
+                    </div>
+                </div>
+                <button type="submit" className="btn btn-primary btn-block">Submit</button>
             </form>
-            </>
+            </div>
         )
 
 }
