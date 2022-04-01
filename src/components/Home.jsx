8f23c31a2b7d4b4a96ebbdd84272c86539f0edd1
anthony <anthonymfinch97@gmail.com>
2022-03-31 21:12:09 -0400

    finished login and register forms

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
+import { Button } from "bootstrap";
 import React, {useState} from "react";
-import {useNavigate} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
-
 import "./homeStyle.css";
 
 const Home = ()=>{
@@ -33,21 +33,25 @@ const Home = ()=>{
                 <h3>Sign In</h3>
                 <div className="error"><p>{error}</p></div>
                 <div className="form-group">
-                    <label htmlFor="user">Name: </label>
-                    <input type="text" className="form-control" name="user" id="user" placeholder="Enter Username or Email" onChange={e =>setUser({...user, username: e.target.value})} value={user.username}/>
+                    <label htmlFor="user">Username/Email: </label>
+                    <input type="text" className="form-control" id="user" placeholder="Username or Email" onChange={e =>setUser({...user, username: e.target.value})} value={user.username}/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="user">Password: </label>
-                    <input type="password" className="form-control" placeholder="Password" name="password" id="password" onChange={e =>setUser({...user, password: e.target.value})} value={user.password} />
+                    <input type="password" className="form-control" placeholder="Password" id="password" onChange={e =>setUser({...user, password: e.target.value})} value={user.password} />
                 </div>
                 <div className="form-group">
                     <div className="custom-control custom-checkbox">
                         <input type="checkbox" className="custom-control-input" id="remember" value={user.remember} onChange={e =>setUser({...user, remember: !user.remember})} />
-                        <label className="custom-control-label" htmlFor="remember">Remember me</label>
+                        <label className="custom-control-label" htmlFor="remember"> Remember me</label>
                     </div>
                 </div>
                 <button type="submit" className="btn btn-primary btn-block">Submit</button>
             </form>
+            <br/>
+            <p>Don't have an account? <br />
+            <Link className="btn btn-secondary btn-sm" role="button" to={"/register"}>Register</Link>
+            </p>
             </div>
         )
 
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,52 +1,57 @@
 import React, {useState} from "react";
 import {Navigate} from "react-router-dom";
-import "./formStyle.css";
+import {Link, useNavigate} from "react-router-dom";
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "./homeStyle.css";
 
 
 const Register = ()=>{
     const [user, setUser] = useState({username:"",email:"" ,password:""});
     const [error, setError] = useState("");
-        const submit = e => {
-            e.preventDefault();
-            console.log(JSON.stringify(user))
-            fetch('/register', { method: 'POST', headers:{'Content-Type':'application/json'} ,body: JSON.stringify(user) })
-                .then(res => res.json())
-                .then(json => {
-                    console.log(json)
-                    var key = Object.keys(json)
-                    console.log(key[0])
-                    if(key[0]==="success"){
-                        return <Navigate to='/'  />;
-                    }
-                    else if(key[0]==="error"){
-                        setError(Object.values(json))
-                    }
-                });
-        }
+    let navigate = useNavigate();
+    const Submit = e => {
+        e.preventDefault();
+        console.log(JSON.stringify(user))
+        fetch('/register', { method: 'POST', headers:{'Content-Type':'application/json'} ,body: JSON.stringify(user) })
+            .then(res => res.json())
+            .then(json => {
+                 console.log(json)
+                var key = Object.keys(json)
+                console.log(key[0])
+                if(key[0]==="success"){
+                    navigate("/");
+                }
+                else if(key[0]==="error"){
+                    setError(Object.values(json))
+                }
+            });
+    }
 
         return (
-            <>
-            <div className="loginBox">
-            <h1>Sign Up</h1>
+            <div className="inner">
+            <form onSubmit={Submit}>
+            <h3>Sign Up</h3>
             {error && <p style={{ color: "red" }}>{error}</p>}
-            <form onSubmit={submit}>
-            <label htmlFor="user">Username: </label>
-                <input type="text" name="user" id="user" onChange={e =>setUser({...user, username: e.target.value})} value={user.username}/>
-                <br></br>
+            <div className="form-group">
+                <label htmlFor="user">Username: </label>
+                <input type="text" className="form-control" id="user" placeholder="Username" onChange={e =>setUser({...user, username: e.target.value})} value={user.username}/>
+            </div>
+            <div className="form-group">
                 <label htmlFor="user">Email: </label>
-                <input type="text" name="email" id="email" onChange={e =>setUser({...user, email: e.target.value})} value={user.email}/>
-                <br></br>
+                <input type="text" className="form-control" id="email" placeholder="Email" onChange={e =>setUser({...user, email: e.target.value})} value={user.email}/>
+            </div>
+            <div className="form-group">
                 <label htmlFor="user">Password: </label>
-                <input type="password" name="password" id="password" onChange={e =>setUser({...user, password: e.target.value})} value={user.password} />
-                <br></br>
-                <input type="submit" name="Sign Up" />
+                <input type="password" className="form-control" placeholder="Password"  id="password" onChange={e =>setUser({...user, password: e.target.value})} value={user.password} />  
+            </div>
+            <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
             </form>
+            <br/>
             <p>
                 Aleady have an account? <br />
-                <a href="/">Log in here</a>
+                <Link className="btn btn-secondary btn-sm" role="button" to={"/"}>Log In</Link>
             </p>
             </div>
-            </>
         )
 
 }
