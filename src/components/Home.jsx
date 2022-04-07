6ec25d2ede67ab97e340b0c4bf159341259753c4
Nathan Larkin <nathanlarkin.usa@gmail.com>
2022-04-06 22:48:28 -0400

    fixed merge conflict

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -24,36 +24,6 @@ const Home = ()=>{
                 });
         }
         
-        return (
-            <div className="inner">
-            <form onSubmit={Submit}>
-                <h3>Sign In</h3>
-                <div className="error"><p>{error}</p></div>
-                <div className="form-group">
-                    <label htmlFor="user">Username/Email: </label>
-                    <input type="text" className="form-control" id="user" placeholder="Username or Email" onChange={e =>setUser({...user, username: e.target.value})} value={user.username}/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="user">Password: </label>
-                    <input type="password" className="form-control" placeholder="Password" id="password" onChange={e =>setUser({...user, password: e.target.value})} value={user.password} />
-                </div>
-                <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input" id="remember" value={user.remember} onChange={e =>setUser({...user, remember: !user.remember})} />
-                        <label className="custom-control-label" htmlFor="remember"> Remember me</label>
-                    </div>
-                </div>
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
-            </form>
-            <br/>
-            <p>Don't have an account? <br />
-            <Link className="btn btn-secondary btn-sm" role="button" to={"/register"}>Register</Link>
-            </p>
-            </div>
-        )
-
-<<<<<<< HEAD
-=======
   return (
     <>
       <div className="error"><p>{error}</p></div>
@@ -73,6 +43,5 @@ const Home = ()=>{
       </form>
     </>
   );
->>>>>>> schema
 }
 export default Home
