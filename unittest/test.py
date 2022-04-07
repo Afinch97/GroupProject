8f4d98f75498f05d5a3dc6b8b211f9e7bf152d33
Fengyun Chen <fchen12@student.gsu.edu>
2022-04-07 16:14:16 -0400

    Update test.py

--- unittest/test.py
+++ unittest/test.py
@@ -8,7 +8,7 @@ class TestApi(unittest.TestCase):
         return super().setUp()
 
     def test_login(self):
-        r = requests.get("http://127.0.0.1:5000/login")
+        r = requests.get("http://127.0.0.1:5000/favs")
         self.assertEqual(r.status_code, 200)
 
 
