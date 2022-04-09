645b93f3811aea08f03d8a8ffa68d4df33207166
anthony <anthonymfinch97@gmail.com>
2022-04-08 22:59:25 -0400

    fixing linting errors

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import { Navbar, Nav,Form, FormControl, Button, Container } from "react-bootstrap";
+import { Navbar, Nav, Form, FormControl, Button, Container } from 'react-bootstrap';
 import { useAuth } from './Auth';
 import './NavBar.css';
 
@@ -8,18 +8,17 @@ function NavBar() {
   const [term, setTerm] = useState('');
   const { signOut } = useAuth();
   let navigate = useNavigate();
-  let location = useLocation()
+  let location = useLocation();
   const Submit = (e) => {
     e.preventDefault();
     // navigate(`/searchy/${term}`)
     window.location = `/${term}`;
   };
   const handleSelect = (eventKey) => {
-    console.log(eventKey)
+    console.log(eventKey);
     navigate(eventKey);
-  }
+  };
   return (
-    <>
     <Navbar bg="light" expand="lg">
     <Container fluid>
       <Navbar.Toggle aria-controls="navbarScroll" />
@@ -28,16 +27,16 @@ function NavBar() {
         style={{ maxHeight: '100px' }}
         navbarScroll >
           <Nav.Item>
-            <Nav.Link eventKey="/searchy">Home</Nav.Link>
+          <Nav.Link eventKey="/searchy">Home</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link eventKey="" href="https://github.com/Afinch97/GroupProject" target="_blank" rel="noreferrer">About</Nav.Link>
+          <Nav.Link eventKey="" href="https://github.com/Afinch97/GroupProject" target="_blank" rel="noreferrer">About</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link eventKey="/favs">Favorites</Nav.Link>
+          <Nav.Link eventKey="/favs">Favorites</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link eventKey="/myComments">Comments</Nav.Link>
+          <Nav.Link eventKey="/myComments">Comments</Nav.Link>
           </Nav.Item>
         </Nav>
         <Form className="d-flex" onSubmit={Submit}>
@@ -46,17 +45,16 @@ function NavBar() {
           placeholder="Search"
           className="me-2"
           aria-label="Search"
-          onChange={e =>setTerm(e.target.value)} 
+          onChange={(e) => setTerm(e.target.value)} 
           value={term}
         />
-        <Button variant="outline-success" type="submit">Search</Button>
+      <Button variant="outline-success" type="submit">Search</Button>
       </Form>
       {' '}
       <Button variant="danger" onClick={signOut}>Logout</Button>
       </Navbar.Collapse>
     </Container>
     </Navbar>
-    </>
   );
 }
 
