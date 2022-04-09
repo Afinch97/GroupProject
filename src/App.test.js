57af1e1451793a833bb857e283dfe4490536aa28
Nathan Larkin <nathanlarkin.usa@gmail.com>
2022-04-08 22:14:39 -0400

    added more client side tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,23 +2,43 @@ import { render, screen, waitFor } from '@testing-library/react';
 import React from 'react';
 import MockAdapter from 'axios-mock-adapter';
 import { flaskClient } from './fetcher';
+import axios from 'axios';
 import App from './App';
 import { UNAUTHENTICATED } from './components/Auth';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 
-const mock = new MockAdapter(flaskClient);
+// const mock = new MockAdapter(axios);
 
-// const mockAuthResponse = {
-//   email: 'test@test.com', is_auth: true, status: 'authenticated', username: 'test',
-// };
+const mockAuthResponse = {
+  email: 'test@test.com',
+  is_auth: true,
+  status: 'authenticated',
+  username: 'test',
+};
 
-// beforeEach(() => {
-//   // suppress console.error calls, that are called from unauthenticated user
-//   // jest.spyOn(console, 'error').mockImplementation(() => {});
-    
-// });
+const server = setupServer(
+  rest.get('/api/auth', (req, res, ctx) => {
+    return res(ctx.json(mockAuthResponse));
+  })
+);
 
-test('renders learn react link', async () => {
-  mock.onGet('/auth').reply(401);
+beforeAll(() => {
+  server.listen();
+});
+afterEach(() => {
+  server.resetHandlers();
+});
+afterAll(() => {
+  server.close();
+});
+
+test('redirects user to login screen when user is not authenticated', async () => {
+  server.use(
+    rest.get('/api/auth', (req, res, ctx) => {
+      return res(ctx.status(401));
+    })
+  );
   render(<App />);
   await waitFor(() => screen.getByRole('button', { name: /submit/i }));
   const button = screen.getByRole('button', { name: /submit/i });
