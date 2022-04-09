cf88899f42ec067189e09413bc3cda24225dd5c6
Nathan Larkin <nathanlarkin.usa@gmail.com>
2022-04-08 23:06:29 -0400

    added api key for tastedive

--- tastedive.py
+++ tastedive.py
@@ -15,14 +15,17 @@ def movie_query_builder(favorite_movies_title: List[str]):
     return ','.join(f'movie:{movie_title}' for movie_title in favorite_movies_title)
 
 
-def get_movie_recommendations(favorite_movies_title: List[str], result_limit: int = 5):
+def get_movie_recommendations(favorite_movies_title: List[str], result_limit: int = 10):
     """based of a list of favorite movie titles, get recommendations based off those movies"""
     params = {
      'q':   movie_query_builder(favorite_movies_title),
      'type': 'movie',
      'info': 1,
-     'limit': result_limit
+     'limit': result_limit,
     }
+    api_key = os.getenv('TASTEDIVE_API_KEY')
+    if api_key is not None:
+        params['k'] = api_key
     response = requests.get(BASE_URL,params)
     response_json = response.json()
     try:
